c97f7a68d2aa6e6fd37a5cadacaeb7c58329ce01
ElyLu <jesuiselayne@gmail.com>
2022-08-26 16:44:28 -0500

    Initial Commit

---
+++ Udder2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder2
+//
+//  Created by Elayne on 26/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Udder2/Udder2App.swift
@@ -0,0 +1,17 @@
+//
+//  Udder2App.swift
+//  Udder2
+//
+//  Created by Elayne on 26/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Udder2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
